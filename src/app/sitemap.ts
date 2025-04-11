937b188e8733276506bfecdf9b109ade35c559e0
Âu Duy Thông <auduythong1610@gmail.com>
2025-04-11 17:21:16 +0700

    Update: dynamic sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,90 +1,91 @@
-import { MetadataRoute } from "next";
+import { getPost } from "@/services/post";
+import { Post } from "@/types/post";
 
-export default function sitemap(): MetadataRoute.Sitemap {
+export const revalidate = 3600;
+
+export default async function sitemap() {
+  const response = await getPost();
+
+  const post = response.posts.map((post: Post) => ({
+    url: `https://writeflow.asia/blog/${post.slug}`,
+    lastModified: new Date(),
+    changeFrequency: "daily",
+    priority: 1,
+  }));
+
   return [
     {
       url: "https://writeflow.asia/opengraph-image.png",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/twitter-image.png",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/blog",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/account/liked-posts",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/account/profile",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/account/my-posts",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/dieu-khoan-su-dung",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/account/settings",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://writeflow.asia/account",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
-      priority: 0.7,
+      priority: 1,
     },
     {
       url: "https://writeflow.asia/chinh-sach-bao-mat",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
-      changeFrequency: "daily",
-      priority: 0.7,
-    },
-    {
-      url: "https://writeflow.asia/viet-bai",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/login",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
     {
       url: "https://writeflow.asia/register",
-      lastModified: new Date("2025-04-10T19:44:34.304Z"),
+      lastModified: new Date(),
       changeFrequency: "daily",
       priority: 0.7,
     },
+    ...post,
   ];
 }
